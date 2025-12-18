2d6542c7321127ef720faad5f756ffcab4a7ac89
Juergen P. <juergenp@core.at>
2025-12-18 18:32:44 +0100

    Update Homebrew formula SHA for v2.0.5

--- Formula/ollamacode.rb
+++ Formula/ollamacode.rb
@@ -7,7 +7,7 @@ class Ollamacode < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/juergengp/ollamaCode/raw/main/bin/ollamacode-arm64"
-      sha256 "319abf69d7ee27ed7e2c8e0d4850c0494899d5ea434439b51ba4ca71b065e406"
+      sha256 "d6a8c28601bd08a0db972d63f06841196c54baaed614a4cdece715a4758c4311"
     else
       url "https://github.com/juergengp/ollamaCode/raw/main/bin/ollamacode-x86_64"
       sha256 "877afacaf5880c47b9283bbf8c8f4ac3cb81b5594f76f02dad2e04d15da6526d"
